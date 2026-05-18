05f4a6f832295d9ac8e1c7707825606a6a39d597
Wisdom Elue <wisdm@juicyway.com>
2026-05-18 15:40:54 +0100

    feat: add ngrok header option

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -11,7 +11,6 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'ngrok-skip-browser-warning': 'true',
   },
   withCredentials: true,
 })
